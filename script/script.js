ccef2cfa6044b822b13974e091260696a57f4767
HTMLCSSExplorer <htmlcssexplorer@gmail.com>
2024-02-25 17:32:08 -0600

    top nav bar for desktop

--- script/script.js
+++ script/script.js
@@ -1,9 +1,15 @@
 const collapsibles = document.querySelectorAll('.collapsible');
 const clickables = document.querySelectorAll('.clickable');
+const topNav = document.querySelector('.top-nav');
+// Collapsibles
 
 clickables.forEach((click) => {
   click.addEventListener('click', (e) => {
     e.target.parentElement.classList.toggle('open');
-    e.target.classList.toggle('open')
+    e.target.classList.toggle('open');
   });
+});
+// Top nav animation
+document.querySelector('.toggler-container').addEventListener('click', () => {
+  topNav.classList.toggle('max-width');
 });
