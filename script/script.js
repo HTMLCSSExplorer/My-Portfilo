8a7da68916f9d99f93680a500031fa7f659d491a
HTMLCSSExplorer <htmlcssexplorer@gmail.com>
2024-02-25 19:45:16 -0600

    adding top nav bar

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 const collapsibles = document.querySelectorAll('.collapsible');
 const clickables = document.querySelectorAll('.clickable');
 const topNav = document.querySelector('.top-nav');
+const togglerImg = document.querySelector('.toggler');
+const togglerContainer = document.querySelector('.toggler-container');
+
 // Collapsibles
 
 clickables.forEach((click) => {
@@ -9,7 +12,33 @@ clickables.forEach((click) => {
     e.target.classList.toggle('open');
   });
 });
+
+function topNavAnimation() {
+  myWindowWith = window.innerWidth;
+  if (myWindowWith > '768px') {
+    togglerContainer.addEventListener('click', () => {
+      topNav.classList.toggle('active');
+      // topNav.classList.add('active');
+    });
+  } else {
+    togglerContainer.addEventListener('click', () => {
+      topNav.classList.toggle('active');
+      topNav.classList.toggle('topnav-max-heith');
+    });
+  }
+}
 // Top nav animation
-document.querySelector('.toggler-container').addEventListener('click', () => {
-  topNav.classList.toggle('max-width');
+let myWindowWith;
+window.addEventListener('DOMContentLoaded', () => {
+  topNavAnimation();
+});
+// window.addEventListener('resize', () => {
+//   topNavAnimation();
+// });
+// console.log(myWindowWith + ' is ');
+
+// Top Nav Menu Toggler Animation
+
+togglerContainer.addEventListener('click', () => {
+  togglerImg.classList.toggle('toggler-active');
 });
